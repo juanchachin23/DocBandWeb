2b8cb536b07c08420090019f0defa361bd84b935
juanchacin23 <juanchacin4@gmail.com>
2024-03-22 02:21:06 -0400

    Front-end de inicio de sesion (listo) (detalles minimos)

---
+++ Registrarse.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- Bootstrap Icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <!-- Conexion con archivo css -->
+    <link href="assets/style.css" rel="stylesheet" >
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+    <title>DocBand-- Registrarse</title>
+
+  </head>
+  <body>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-12 col-sm-12 col-xs-12">
+        <form class="mx-auto">
+          <div id="prueba">
+               <i class="bi bi-person-circle"></i> 
+            
+          </div>
+          <div class="formulario">
+            <h4 class="text-center">DocBand</h4>
+            <div>
+                  <label for="exampleInputEmail1" class="form-label">Usuario</label>
+                  <input type="email" class="form-control"  aria-describedby="emailHelp" placeholder="Coloque su usuario">
+            </div>
+            <div>
+                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                <input type="password" class="form-control"  placeholder="coloque su contraseña">
+            </div>
+                
+                <div class="row">
+                <div class="col-lg-6 col-md-6 col-6 col-sm-6 col-xs-6">
+                
+                    <a>¿Olvidaste tu contraseña?</a> 
+                </div>
+                <div class="col-lg-6 col-md-6 col-6 col-sm-6 col-xs-6" id="Text-Align">
+                
+                    <a href="inicio-de-sesion.php">volver a inicio de sesion</a> 
+                </div>
+                
+                </div>
+                
+                
+                <button type="submit" class="btn btn-primary mt-5">Registrarse</button>
+          </div>
+              </form>
+    </div>
+      </div>
+      </div>
+
+        
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docband</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/1ab94d0eba.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+
+</body>
+</html>
